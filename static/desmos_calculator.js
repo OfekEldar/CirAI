9bf1f2aca7d1aa60fec0c85f32bf8b69a6c7f06b
OfekEldar <ofek147111@gmail.com>
2026-02-15 00:06:51 +0200

    f

--- static/desmos_calculator.js
+++ static/desmos_calculator.js
@@ -168,7 +168,7 @@ class DesmosCalculatorManager {
             {id: 'f', latex: 'Z = \\frac{1}{1+sR_{e}C_{e}}'},
             {id: 'Re', latex: 'R_{e} = 10k'},
             {id: 'Ce', latex: 'C_{e} = 1p'},
-            {id: 'z_val', latex: `Z = ${this.zLatex}`},
+            /*{id: 'z_val', latex: `Z = ${this.zLatex}`},*/
             {id: 'z_abs', latex: '\\left|Z\\right|'},
             {id: 's_def', latex: 's = i * 2 * \\pi * x'}
         ];
